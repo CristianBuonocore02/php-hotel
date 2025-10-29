6bb901cb624f584ac66d8b3281071835cdcf3e15
Cristian Buonocore <cristian.buonocore680@gmail.com>
2025-10-29 14:33:00 +0100

    Added filters for parking and voting

--- index.php
+++ index.php
@@ -67,6 +67,28 @@
 
     <h1>Hotels</h1>
 
+    <form method="GET" class="container mt-4 mb-4">
+        <div class="row g-3 align-items-center justify-content-center">
+            <div class=" form-control">
+                <label for="parking" class="form-label">Solo con parcheggio:</label>
+                <input type="checkbox" for="parking" name="parking" id="parking" value="1"
+                    <?php if (isset($_GET['parking'])) echo 'checked'; ?>>
+            </div>
+
+            <div class="form-control">
+                <label for="vote" class="form-label">Voto minimo:</label>
+                <input type="number" name="vote" id="vote" class="form-control" min="1" max="5"
+                    value="<?php echo $_GET['vote'] ?? ''; ?>">
+            </div>
+
+            <div class="col-auto">
+                <button type="submit" class="btn btn-primary">Filtra</button>
+                <a href="<?php echo $_SERVER['PHP_SELF']; ?>" class="btn btn-secondary">Reset</a>
+            </div>
+        </div>
+    </form>
+
+
     <table class="mt-5 m-auto">
         <thead>
             <tr class="border">
@@ -81,13 +103,46 @@
 
             <?php
 
-            foreach ($hotels as $hotel) {
+            // Applica i filtri se sono stati inviati
+            // Inizializza i filtri
+            $parking_requested = false;
+            $minVote = 0;
+
+            // Controlla se l'utente ha selezionato il checkbox del parcheggio
+            if (isset($_GET['parking']) && $_GET['parking'] == "1") {
+                $parking_requested = true;
+            }
+
+            // Controlla se l'utente ha inserito un voto minimo
+            if (!empty($_GET['vote'])) {
+                $minVote = (int) $_GET['vote'];
+            }
+
+            // Applica i filtri
+            $filteredHotels = array_filter($hotels, function ($hotel) use ($parking_requested, $minVote) {
+                // Filtro parcheggio
+                if ($parking_requested && !$hotel['parking']) {
+                    return false;
+                }
+
+                // Filtro voto minimo
+                if ($hotel['vote'] < $minVote) {
+                    return false;
+                }
+
+                // Se passa entrambi i filtri
+                return true;
+            });
+
+            // Mostra gli hotel filtrati
+            foreach ($filteredHotels as $hotel) {
+
 
             ?>
                 <tr class="border">
                     <td><?php echo $hotel['name'] ?></td>
                     <td><?php echo $hotel['description'] ?></td>
-                    <td><?php echo $hotel['parking'] ?></td>
+                    <td><?php echo $hotel['parking'] ? 'Sì' : 'No'; ?></td>
                     <td><?php echo $hotel['vote'] ?></td>
                     <td><?php echo $hotel['distance_to_center'] ?></td>
                 </tr>
